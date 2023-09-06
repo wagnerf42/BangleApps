67b9b7dc98a2d0d2465ac6f1e972795f5daa8d90
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2023-09-06 17:15:17 +0200

    bhack: basic mechanic

--- apps/bhack/app.js
+++ apps/bhack/app.js
@@ -70,17 +70,90 @@ function generate_room(rooms, width, height) {
     // }
 }
 
-const KNIGHT = 340;
+const MONSTERS = [undefined, "Player", "Newt"];
+// images are tiles numbers : 340, 326
+const MONSTERS_IMAGES = [
+    undefined,
+    require("heatshrink").decompress(atob("kEggmqiIACgFEAAIEBBIeqB58RokzmYOBmf/AoIQBAYIVBB6AOCBAIOCGIIPCCgIPRAwJJEHAIPX/4wCAoPdJ4YPX93uGoQACB63dFIXdKoQPVFIVVAAK0BCAIPVAALPDogCBJ4wPPJIILCB4IBBB6xKBegVEJgPdBAIPTSAQGCdwVVN4oPRAAcRNwgPSE4JHBFATtEKoQPP1QLBAAbMCqqXCBwIPPgAQBAAhUFBwIPP")),
+    require("heatshrink").decompress(atob("kEggmqiIAM1QPPiNEmYAKogRBB54HEJwoICB6sAqvdAgPdqoQCB6gOBFoYTBCAQPVJIYFBGIIPXCYlVB7ANEOIJPIB6CffBoYOBK4YPTBwlVWYRvGB5wOCAQJLCAIQPVNgIoBFggACB6NEEwQKCeYtEB6GqCAIAKBwIPPgAQBABQOBB54A=="))
+];
+
+const KNIGHT = 1;
 const FLOOR = 848;
-const NEWT = 326;
+const NEWT = 2;
 
 // brightness 0, contrast 70
 let floor_img = require("heatshrink").decompress(atob("kEggmqiIAM1QPPiNEmYAKogRBB54OLAAIP/B/4P/B/4P/B6NEBxdEB6GqCAIAKBwIPPgAQBABQOBB54="));
-let knight_img = require("heatshrink").decompress(atob("kEggmqiIACgFEAAIEBBIeqB58RokzmYOBmf/AoIQBAYIVBB6AOCBAIOCGIIPCCgIPRAwJJEHAIPX/4wCAoPdJ4YPX93uGoQACB63dFIXdKoQPVFIVVAAK0BCAIPVAALPDogCBJ4wPPJIILCB4IBBB6xKBegVEJgPdBAIPTSAQGCdwVVN4oPRAAcRNwgPSE4JHBFATtEKoQPP1QLBAAbMCqqXCBwIPPgAQBAAhUFBwIPP"));
-let newt_img = require("heatshrink").decompress(atob("kEggmqiIAM1QPPiNEmYAKogRBB54HEJwoICB6sAqvdAgPdqoQCB6gOBFoYTBCAQPVJIYFBGIIPXCYlVB7ANEOIJPIB6CffBoYOBK4YPTBwlVWYRvGB5wOCAQJLCAIQPVNgIoBFggACB6NEEwQKCeYtEB6GqCAIAKBwIPPgAQBABQOBB54A=="));
+
+class Creature {
+    constructor(monster_type, x, y) {
+        if (monster_type == KNIGHT) {
+            this.hp = 10;
+            this.ac = 10;
+            this.attack_modifier = 4;
+            this.speed = 6;
+            this.damages = [1,6,0];
+        } else if (monster_type == NEWT) {
+            this.hp = 4;
+            this.ac = 10;
+            this.attack_modifier = 4;
+            this.speed = 8;
+            this.damages = [1,4,0];
+        } else {
+            console.log("unknown monster");
+        }
+        this.max_hp = this.hp;
+        this.x = x;
+        this.y = y;
+        this.monster_type = monster_type;
+    }
+    name() {
+        return MONSTERS[this.monster_type];
+    }
+    move() {
+        let player_x = game.player.x;
+        let player_y = game.player.y;
+        let xdiff = Math.abs(player_x - this.x);
+        let ydiff = Math.abs(player_y - this.y);
+        if ((xdiff == 1 && ydiff == 0)||(xdiff == 0 && ydiff == 1)) {
+            console.log("player_x", player_x, "player_y", player_y, "xdiff", xdiff, "ydiff", ydiff);
+            // we are just beside player, attack
+            this.attack(game.player, [player_x, player_y]);
+        } else {
+            console.log("TODO: move towards player");
+        }
+    }
+    attack(target, position) {
+        if (randint(1, 20) + this.attack_modifier >= target.ac) {
+            // attack success
+            let damages = this.damages[2];
+            for (let i=0 ; i < this.damages[0] ; i++) {
+                damages += randint(1, this.damages[1]);
+            }
+            target.hp -= damages;
+            if (target.hp < 0) {
+                // we kill it
+                msg = target.name() + " killed";
+                game.monsters = game.monsters.filter(m => m.hp > 0);
+                game.map.set_cell(position, FLOOR);
+                game.display();
+            } else {
+                msg = target.name() + " hit(" + damages + ")";
+                if (target.monster_type == KNIGHT) {
+                    game.display_hp();
+                }
+            }
+        } else {
+            // attack miss
+            msg = target.name() + " missed";
+        }
+        g.setColor(0, 0, 0).fillRect(0, 32*5, 32*5, g.getHeight());
+        g.setColor(1, 1, 1).setFont("6x8:2").setFontAlign(-1, 1, 0).drawString(msg, 0, g.getHeight());
+    }
+}
 
 class Map {
-    constructor(width, height) {
+    constructor(width, height, monsters, player) {
         this.width = width;
         this.height = height;
         this.seed = E.hwRand();
@@ -95,31 +168,27 @@ class Map {
         }
         this.generate_corridors(rooms);
         let first_room = rooms[0];
-        this.player_position = [
-            randint(first_room[0], first_room[0]+first_room[2]-1),
-            randint(first_room[1], first_room[1]+first_room[3]-1)
-        ];
-        this.set_cell(this.player_position, KNIGHT);
-        this.generate_monsters(rooms);
+        player.x = randint(first_room[0], first_room[0]+first_room[2]-1);
+        player.y = randint(first_room[1], first_room[1]+first_room[3]-1);
+        this.set_cell([player.x, player.y], KNIGHT);
+        this.generate_monsters(rooms, monsters, player);
     }
-    generate_monsters(rooms) {
-        this.monsters = [];
+    generate_monsters(rooms, monsters, player) {
         for (let i=0 ; i < rooms.length ; i++) {
             let room = rooms[i];
             let nx;
             if (i == 0) {
                 nx = randint(room[0], room[0]+room[2]-2);
-                if (nx >= this.player_position[0]) {
+                if (nx >= player.x) {
                     nx += 1;
                 }
             } else {
                 nx = randint(room[0], room[0]+room[2]-1);
             }
             let ny = randint(room[1], room[1]+room[3]-1);
-            this.monsters.push({type: NEWT, position: [nx, ny]});
+            monsters.push(new Creature(NEWT, nx, ny));
             this.set_cell([nx, ny], NEWT);
         }
-        
     }
     fill_room(room) {
         for (let x=room[0]; x<room[0]+room[2]; x++) {
@@ -179,47 +248,97 @@ class Map {
         }
     }
     display() {
-        g.setBgColor(0, 0, 0);
-        g.clear();
         g.setColor(0.2, 0.2, 0.2);
         g.fillRect(0, 0, 32*5, 32*5);
-        let player = this.player_position;
         for(let x=-2; x<=2; x++) {
             for(let y=-2; y<=2; y++) {
-                let pos = [player[0]+x, player[1]+y];
+                let pos = [game.player.x+x, game.player.y+y];
                 let content = this.get_cell(pos);
                 if (content == FLOOR) {
                     g.drawImage(floor_img, (x+2)*32, (y+2)*32);
-                } else if (content == KNIGHT) {
-                    g.drawImage(knight_img, (x+2)*32, (y+2)*32);
-                } else if (content == NEWT) {
-                    g.drawImage(newt_img, (x+2)*32, (y+2)*32);
+                } else if ((content !== undefined) && (content != 0)) {
+                    g.drawImage(MONSTERS_IMAGES[content], (x+2)*32, (y+2)*32);
                 }
             }
         }
     }
-    move(direction) {
-        let new_position = go(this.player_position, direction);
-        if (!this.valid_position(new_position)) {
-            return;
-        }
-        if (this.get_cell(new_position) == 0) {
-            return;
-        }
-        this.set_cell(this.player_position, FLOOR);
+    move(new_position) {
+        this.set_cell([game.player.x, game.player.y], FLOOR);
         this.set_cell(new_position, KNIGHT);
-        this.player_position = new_position;
-        map.display();
+        game.player.x = new_position[0];
+        game.player.y = new_position[1];
     }
 }
 
-let in_menu = false;
-let map = new Map(30, 30);
-map.display();
-Bangle.setLocked(false);
+
+class Game {
+    constructor() {
+        this.locked = false;
+        this.monsters = [];
+        this.player = new Creature(KNIGHT);
+        this.map = new Map(30, 30, this.monsters, this.player);
+        this.time = 0;
+        Bangle.setLocked(false);
+    }
+    display() {
+        this.map.display();
+    }
+    display_hp() {
+        let hp_y = g.getHeight() - Math.round((this.player.hp * g.getHeight())/this.player.max_hp);
+        g.setColor(0, 0, 0).fillRect(5*32, 0, g.getWidth(), g.getHeight());
+        g.setColor(1, 0, 0).fillRect(5*32, hp_y, g.getWidth(), g.getHeight());
+        g.setFont("4x6:2").setColor(0, 0, 0).setFontAlign(-1, 1, 0).drawString(this.player.hp, 5*32, g.getHeight());
+    }
+    attack(position) {
+        let monster = this.monsters.find(m => m.x == position[0] && m.y == position[1]);
+        this.player.attack(monster, position);
+    }
+    move(direction) {
+        let destination = go([this.player.x, this.player.y], direction);
+        if (!this.map.valid_position(destination)) {
+            return;
+        }
+        let destination_content = this.map.get_cell(destination);
+        if (destination_content == 0) {
+            return;
+        }
+        if (destination_content < 100) {
+            // attack
+            this.attack(destination);
+        } else {
+            // move
+            this.map.move(destination);
+            this.display();
+        }
+        this.advance_time();
+    }
+    advance_time() {
+        this.locked = true;
+        while(true) {
+            this.time += 1;
+            this.monsters.forEach(monster => {
+                if (this.time % monster.speed == 0) {
+                    monster.move();
+                }
+            });
+            if (this.time % this.player.speed == 0) {
+                this.locked = false;
+                return;
+            }
+        }
+    }
+}
+
+let game = new Game();
+
+g.setBgColor(0, 0, 0);
+g.clear();
+game.display();
+game.display_hp();
 
 Bangle.on('touch', function(button, xy) {
-    if (in_menu) {
+    if (game.locked) {
+        console.log("locked");
         return;
     }
     let half_width = g.getWidth() / 2;
@@ -250,7 +369,7 @@ Bangle.on('touch', function(button, xy) {
     if (directions_amplitudes[max_direction] - directions_amplitudes[second_max_direction] > 10) {
         // if there is little possible confusions between two candidate moves let's move.
         // basically we forbid diagonals of 10 pixels wide
-        map.move(max_direction);
+        game.move(max_direction);
     }
 
 });
