9e3f6a53085686f6867e21ed1aa3b47b8fa1c8b2
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2024-05-29 18:05:53 +0200

    bhack: adding conversion script

---
+++ apps/bhack/convert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from os import system
+
+def generate(ids, filename):
+    files = ("nethack_3.4.3/hack{:0>3}.png".format(i) for i in ids)
+    command = "convert " + " ".join(files) + " -append " + filename
+    print(command)
+    system(command)
+
+
+def main():
+    monsters = [340, 326, 0, 20, 71, 42, 95, 73]
+    generate(monsters, "monsters.png")
+    special_items = [786, 660, 664, 586]
+    generate(special_items, "special_items.png")
+    items = [411, 465, 533, 423, 450, 412, 541, 525, 510, 574]
+    generate(items, "items.png")
+    misc = [852, 856, 859]
+    generate(misc, "misc.png")
+    levels = [848, 823, 830, 831, 832, 833, 834, 835, 837, 838, 839, 840, 836]
+    generate(levels, "levels.png")
+    generate(levels+monsters+special_items+items+misc, "all.png")
+    print("first monster:", len(levels))
+    print("first special:", len(levels)+len(monsters))
+    print("first item:", len(levels)+len(monsters)+len(special_items))
+    print("first misc:", len(levels)+len(monsters)+len(special_items)+len(items))
+    print("void:", len(levels)+len(monsters)+len(special_items)+len(items)+len(misc))
+
+
+main()
