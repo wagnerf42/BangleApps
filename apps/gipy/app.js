d29b7341366058856ef9bf3bb98afc32f9ab3b43
frederic wagner <frederic.wagner@imag.fr>
2025-04-18 12:54:11 +0200

    gipy: small fix for recent changes

--- apps/gipy/app.js
+++ apps/gipy/app.js
@@ -1426,144 +1426,6 @@ function start(fn) {
   ask_options(fn);
 }
 
-function options_menu() {
-  status.activate();
-  if (in_menu) {
-    return;
-  }
-  in_menu = true;
-  const menu = {
-    "": {
-      title: "choose action",
-    },
-    "Go Backward": {
-      value: go_backwards,
-      format: (v) => (v ? "On" : "Off"),
-      onchange: (v) => {
-        go_backwards = v;
-      },
-    },
-    Zoom: {
-      value: status.zoomed_in,
-      format: (v) => (v ? "In" : "Out"),
-      onchange: (v) => {
-        status.zoomed_in = v;
-        status.reset_images_cache();
-      },
-    },
-    /*LANG*/
-    powersaving: {
-      value: powersaving,
-      onchange: (v) => {
-        powersaving = v;
-      },
-    },
-    "back to map": function () {
-      in_menu = false;
-      E.showMenu();
-      g.clear();
-      g.flip();
-      if (status !== null) {
-        status.display();
-      }
-    },
-    "exit": function() { load() },
-  };
-  try {
-    // plot openstmap option if installed
-    const osm = require("openstmap");
-    menu[/*LANG*/"Plot OpenStMap"] = function() {
-      E.showMenu(); // remove menu
- 
-      // compute min/max coordinates
-      const fix = Bangle.getGPSFix();
-      let minLat = fix.lat ? fix.lat : 90;
-      let maxLat = fix.lat ? fix.lat : -90;
-      let minLong = fix.lon ? fix.lon : 180;
-      let maxLong = fix.lon ? fix.lon : -180;
-      for(let i=0; i<path.len; i++) {
-        const point = path.point(i);
-        if(point.lat>maxLat) maxLat=point.lat; if(point.lat<minLat) minLat=point.lat;
-        if(point.lon>maxLong) maxLong=point.lon; if(point.lon<minLong) minLong=point.lon;
-      }
-      const max = Bangle.project({lat: maxLat, lon: maxLong});
-      const min = Bangle.project({lat: minLat, lon: minLong});
-      const scaleX = (max.x-min.x)/Bangle.appRect.w;
-      const scaleY = (max.y-min.y)/Bangle.appRect.h;
- 
-      // openstmap initialization
-      osm.scale = Math.ceil((scaleX > scaleY ? scaleX : scaleY)*1.1); // add 10% margin
-      osm.lat = (minLat+maxLat)/2.0;
-      osm.lon = (minLong+maxLong)/2.0;
- 
-      const drawOpenStmap = () => {
-        g.clearRect(Bangle.appRect);
-        osm.draw();
- 
-        // draw track
-        g.setColor("#f09");
-        for(let i=0; i<path.len; i++) {
-          const point = path.point(i);
-          const mp = osm.latLonToXY(point.lat, point.lon);
-          if (i == 0) {
-            g.moveTo(mp.x,mp.y);
-          } else {
-            g.lineTo(mp.x,mp.y);
-          }
-          g.fillCircle(mp.x,mp.y,2); // make the track more visible
-        }
- 
-        // draw current position
-        g.setColor("#000");
-        if (fix.lat && fix.lon) {
-          const icon = require("heatshrink").decompress(atob("jEYwYPMyVJkgHEkgICyAHCgIIDyQIChIIEoAIDC4IIEBwOAgEEyVIBAY4DBD4sGHxBQIMRAIIPpAyCHAYILUJEAiVJkAIFgVJXo5fCABQA==")); // 24x24px
-          const mp = osm.latLonToXY(fix.lat, fix.lon);
-          g.drawImage(icon, mp.x, mp.y);
-        }
- 
-        // labels
-        g.setFont("6x8",2);
-        g.setFontAlign(0,0,3);
-        g.drawString(/*LANG*/"Back", g.getWidth() - 10, g.getHeight()/2);
-        g.drawString("+", g.getWidth() - 10, g.getHeight()/4);
-        g.drawString("-", g.getWidth() - 10, g.getHeight()/4*3);
-      };
-      drawOpenStmap();
- 
-      let startDrag = 0;
-      Bangle.setUI({
-        mode: "custom",
-        btn: (n) => { // back handling
-          g.clearRect(0, 0, g.getWidth(), g.getHeight());
-          E.showMenu(menu);
-        },
-        drag: (ev) => { // zoom, move
-          if (ev.b) {
-            osm.scroll(ev.dx, ev.dy);
-            if (!startDrag) {
-              startDrag = getTime();
-            }
-          } else {
-            if (getTime() - startDrag < 0.2) {
-              // tap
-              if (ev.y > g.getHeight() / 2) {
-                osm.scale *= 2;
-              } else {
-                osm.scale /= 2;
-              }
-            }
-            startDrag = 0;
-            drawOpenStmap();
-          }
-        },
-      });
-    };
-  } catch (ex) {
-    // openstmap not available.
-  }
-  E.showMenu(menu);
-}
-
 
 function start_gipy(path, maps, interests, heights) {
   console.log("starting");
@@ -1575,6 +1437,147 @@ function start_gipy(path, maps, interests, heights) {
   console.log("creating status");
   status = new Status(path, maps, interests, heights);
   console.log("done creating status");
+
+  let path = status.path;
+  function options_menu() {
+    status.activate();
+    if (in_menu) {
+      return;
+    }
+    in_menu = true;
+    const menu = {
+      "": {
+        title: "choose action",
+      },
+      "Go Backward": {
+        value: go_backwards,
+        format: (v) => (v ? "On" : "Off"),
+        onchange: (v) => {
+          go_backwards = v;
+        },
+      },
+      Zoom: {
+        value: status.zoomed_in,
+        format: (v) => (v ? "In" : "Out"),
+        onchange: (v) => {
+          status.zoomed_in = v;
+          status.reset_images_cache();
+        },
+      },
+      /*LANG*/
+      powersaving: {
+        value: powersaving,
+        onchange: (v) => {
+          powersaving = v;
+        },
+      },
+      "back to map": function () {
+        in_menu = false;
+        E.showMenu();
+        g.clear();
+        g.flip();
+        if (status !== null) {
+          status.display();
+        }
+      },
+      "exit": function() { load() },
+    };
+    try {
+      // plot openstmap option if installed
+      const osm = require("openstmap");
+      menu[/*LANG*/"Plot OpenStMap"] = function() {
+        E.showMenu(); // remove menu
+   
+        // compute min/max coordinates
+        const fix = Bangle.getGPSFix();
+        let minLat = fix.lat ? fix.lat : 90;
+        let maxLat = fix.lat ? fix.lat : -90;
+        let minLong = fix.lon ? fix.lon : 180;
+        let maxLong = fix.lon ? fix.lon : -180;
+        for(let i=0; i<path.len; i++) {
+          const point = path.point(i);
+          if(point.lat>maxLat) maxLat=point.lat; if(point.lat<minLat) minLat=point.lat;
+          if(point.lon>maxLong) maxLong=point.lon; if(point.lon<minLong) minLong=point.lon;
+        }
+        const max = Bangle.project({lat: maxLat, lon: maxLong});
+        const min = Bangle.project({lat: minLat, lon: minLong});
+        const scaleX = (max.x-min.x)/Bangle.appRect.w;
+        const scaleY = (max.y-min.y)/Bangle.appRect.h;
+   
+        // openstmap initialization
+        osm.scale = Math.ceil((scaleX > scaleY ? scaleX : scaleY)*1.1); // add 10% margin
+        osm.lat = (minLat+maxLat)/2.0;
+        osm.lon = (minLong+maxLong)/2.0;
+   
+        const drawOpenStmap = () => {
+          g.clearRect(Bangle.appRect);
+          osm.draw();
+   
+          // draw track
+          g.setColor("#f09");
+          for(let i=0; i<path.len; i++) {
+            const point = path.point(i);
+            const mp = osm.latLonToXY(point.lat, point.lon);
+            if (i == 0) {
+              g.moveTo(mp.x,mp.y);
+            } else {
+              g.lineTo(mp.x,mp.y);
+            }
+            g.fillCircle(mp.x,mp.y,2); // make the track more visible
+          }
+   
+          // draw current position
+          g.setColor("#000");
+          if (fix.lat && fix.lon) {
+            const icon = require("heatshrink").decompress(atob("jEYwYPMyVJkgHEkgICyAHCgIIDyQIChIIEoAIDC4IIEBwOAgEEyVIBAY4DBD4sGHxBQIMRAIIPpAyCHAYILUJEAiVJkAIFgVJXo5fCABQA==")); // 24x24px
+            const mp = osm.latLonToXY(fix.lat, fix.lon);
+            g.drawImage(icon, mp.x, mp.y);
+          }
+   
+          // labels
+          g.setFont("6x8",2);
+          g.setFontAlign(0,0,3);
+          g.drawString(/*LANG*/"Back", g.getWidth() - 10, g.getHeight()/2);
+          g.drawString("+", g.getWidth() - 10, g.getHeight()/4);
+          g.drawString("-", g.getWidth() - 10, g.getHeight()/4*3);
+        };
+        drawOpenStmap();
+   
+        let startDrag = 0;
+        Bangle.setUI({
+          mode: "custom",
+          btn: (n) => { // back handling
+            g.clearRect(0, 0, g.getWidth(), g.getHeight());
+            E.showMenu(menu);
+          },
+          drag: (ev) => { // zoom, move
+            if (ev.b) {
+              osm.scroll(ev.dx, ev.dy);
+              if (!startDrag) {
+                startDrag = getTime();
+              }
+            } else {
+              if (getTime() - startDrag < 0.2) {
+                // tap
+                if (ev.y > g.getHeight() / 2) {
+                  osm.scale *= 2;
+                } else {
+                  osm.scale /= 2;
+                }
+              }
+              startDrag = 0;
+              drawOpenStmap();
+            }
+          },
+        });
+      };
+    } catch (ex) {
+      // openstmap not available.
+    }
+    E.showMenu(menu);
+  }
+
+
 
   setWatch(
     options_menu,
@@ -1599,7 +1602,6 @@ function start_gipy(path, maps, interests, heights) {
   status.display();
 
   Bangle.on("touch", (button, xy) => {
-    let active = status.active;
     status.activate();
     if (in_menu) {
       return;
